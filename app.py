526df17957ce20d4c9e1c37ac4936eec14b29cd2
Goutham <dsgr369@gmail.com>
2021-02-06 18:46:20 +0530

    User Auth added

--- app.py
+++ app.py
@@ -1,42 +1,46 @@
-from flask import Flask, render_template, redirect, url_for
-from forms import registration, loginForm
-from flask_login import LoginManager, login_required
+from flask import Flask, redirect, render_template, url_for
+from flask.helpers import flash
+from flask_login import LoginManager, login_required, UserMixin, login_user, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import create_engine, insert, select
+from forms import loginForm, registration
+from werkzeug.security import generate_password_hash, check_password_hash
+
 app=Flask(__name__)
 app.config['SECRET_KEY']="AA1D1644C4313521A74D6F6D75966"
-#login_manager=LoginManager()
-#login_manager.init_app(app)
+login_manager=LoginManager()
+login_manager.init_app(app)
+login_manager.login_view='login'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///auth.db'
-
 db=SQLAlchemy(app)
-dbengine=create_engine('sqlite:///auth.db')
+
 # Models
-class details(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(30), nullable=False)
     userID = db.Column(db.String(20), unique=True, nullable=False)
     emailID = db.Column(db.String(120), unique=True, nullable=False)
     mobileNo = db.Column(db.Integer, unique=True, nullable=False)
-class cred(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    userID = db.Column(db.String(20), unique=True, nullable=False)
     passwd=db.Column(db.String(20), nullable=False)
 
-@app.route("/")
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/home")
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('index.html', name=current_user.name)
 
 @app.route("/register", methods=['POST', 'GET'])
 def register():
     reg_form=registration()
     if reg_form.validate_on_submit():
-        #detail=details(name=reg_form.name, userID=reg_form.userID, emailID= reg_form.emailID, mobileNo=reg_form.mobileNo)
-        #creds=cred(userID= reg_form.userID, passwd=reg_form.passwd)
-        
-    
-        dbengine.execute("insert into details(name, userID, emailID, mobileNo) values ('{}', '{}', '{}', {});".format(reg_form.name.data, reg_form.userID.data, reg_form.emailID.data, reg_form.mobileNo.data))
-        dbengine.execute("insert into cred(userID, passwd) values ('{}', '{}');".format(reg_form.userID.data, reg_form.passwd.data))
+        hashPass=generate_password_hash(reg_form.passwd.data, method='sha256')
+        user=User(name=reg_form.name.data, userID=reg_form.userID.data\
+            , emailID= reg_form.emailID.data, mobileNo=reg_form.mobileNo.data, passwd=hashPass)
+        db.session.add(user)
+        db.session.commit()
+        flash('Account Created Successfully', 'success')
         return redirect(url_for('login'))
     return render_template('register.html', form=reg_form)
 
@@ -44,15 +48,20 @@ def register():
 def login():
     form=loginForm()
     if form.validate_on_submit():
-        try:
-            data=list(dbengine.execute("select userID, passwd from cred where userID='{}';".format(form.userID.data)))
-            if data[0][0]==form.userID.data:
-                if data[0][1]==form.passwd.data:
-                    return redirect('/')
-        except:
-            pass
+        user=User.query.filter_by(userID=form.userID.data).first()
+        if user:
+            if check_password_hash(user.passwd, form.passwd.data):
+                login_user(user)
+                return redirect(url_for('index'))
+        flash('Invalid Credentials', 'error')
         return redirect(url_for('login'))
     return render_template('login.html', form=form)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 if __name__=="__main__":
     app.run(debug=True)
